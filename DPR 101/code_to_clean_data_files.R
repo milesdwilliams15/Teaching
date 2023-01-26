2e29ce7fb896835411503b64a106b76f550b2b7a
milesdwilliams15 <milesdwilliams15@gmail.com>
2023-01-26 09:29:10 -0500

    let's try this!

---
+++ DPR 101/code_to_clean_data_files.R
@@ -0,0 +1,38 @@
+# Cross-reference governance with civil war
+rm(list = ls())
+library(tidyverse)
+library(peacesciencer)
+library(wwmetrics)
+library(countrycode)
+
+ucdp_onsets %>%
+  transmute(
+    country = countrycode(
+      gwcode, "gwn", "country.name"
+    ),
+    year,
+    sumonset1
+  ) %>%
+  distinct() -> onset
+
+data("WGI")
+WGI %>%
+  transmute(
+    country = countrycode(
+      Country, "iso3c", "country.name"
+    ),
+    year,
+    wgi = mean_WGI
+  ) -> wgi
+
+left_join(
+  onset, wgi,
+  by = c("country", "year")
+) -> Data
+
+write_csv(
+  Data,
+  here::here(
+    "DPR 101", "Data", "onset_and_wgi.csv"
+  )
+)
