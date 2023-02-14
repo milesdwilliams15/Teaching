d6d60a9fbc71bad155783bee4c728bc632058770
milesdwilliams15 <milesdwilliams15@gmail.com>
2023-02-14 15:14:58 -0500

    dpr 101

--- DPR 101/Data/prep_data.R
+++ DPR 101/Data/prep_data.R
@@ -3,19 +3,21 @@
 library(tidyverse)
 url <- "https://raw.githubusercontent.com/fivethirtyeight/checking-our-work-data/master/presidential_elections.csv"
 Data <- read_csv(url)
-str(Data)
+glimpse(Data)
 
 Data <- Data %>%
   filter(forecast_type == "polls-plus",
-         party == "D") %>%
-  select(forecast_date, state, projected_voteshare, actual_voteshare, probwin, probwin_outcome)
+         candidate %in% c("Biden", "Clinton"),
+         is.na(district)) %>%
+  select(forecast_date, state, projected_voteshare, 
+         actual_voteshare, probwin, probwin_outcome)
 
 write_csv(
   Data,
   here::here("DPR 101", "Data", "538_prez_forecast.csv")
 )
 
-Data <- filter(
+small_Data <- filter(
   Data,
   forecast_date %in% as.Date(c("2020-09-01",
                                "2020-10-01",
@@ -23,6 +25,33 @@ Data <- filter(
 )
 
 write_csv(
-  Data,
+  small_Data,
   here::here("DPR 101", "Data", "538_prez_forecast_small_2020.csv")
+)
+
+change_Data <- Data %>%
+  mutate(
+    election = ifelse(
+      forecast_date < as.Date("2020-01-01"),
+      "2016",
+      "2020"
+    )
+  ) %>%
+  group_by(
+    election, state
+  ) %>%
+  summarize(
+    projected_voteshare = projected_voteshare[
+      forecast_date == max(forecast_date)
+    ],
+    actual_voteshare = unique(actual_voteshare)
+  ) %>%
+  ungroup %>%
+  mutate(
+    forecast_error = projected_voteshare - actual_voteshare
+  )
+
+write_csv(
+  change_Data,
+  here::here("DPR 101", "Data", "538_prez_forecast_error_20162020.csv")
 )
