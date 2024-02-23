d91da5922ffe6af419ea413d41bd8d9f6d3c89b2
milesdwilliams15 <milesdwilliams15@gmail.com>
2024-02-23 10:30:01 -0500

    fixing the data

--- DPR 101/Main Assignments/fix_ma1_data.R
+++ DPR 101/Main Assignments/fix_ma1_data.R
@@ -4,6 +4,12 @@ library(tidyverse)
 ## read in the data
 url <- "http://tinyurl.com/ma1dataset"
 Data <- read_csv(url)
+Data |>
+  mutate(
+    county_name = str_remove_all(
+      county_name, " County"
+    )
+  ) -> Data
 
 ## bring in county map data
 library(socviz)
@@ -24,10 +30,17 @@ map_data |>
 Data |>
   mutate(
     county_name = paste0(county_name, " County")
-  ) |>
+  ) -> Data
+
+Data |>
   left_join(
     map_ids,
     by = c("county_name" = "name")
+  ) -> Data
+
+Data |>
+  mutate(
+    id = as.character(id)
   ) -> Data
 
 write_csv(
