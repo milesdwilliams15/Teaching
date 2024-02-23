3b02126936dc104323687c54d9ecfae26d011431
milesdwilliams15 <milesdwilliams15@gmail.com>
2024-02-23 10:01:26 -0500

    please work

---
+++ DPR 101/Main Assignments/fix_ma1_data.R
@@ -0,0 +1,40 @@
+
+## open the {tidyverse}
+library(tidyverse)
+## read in the data
+url <- "http://tinyurl.com/ma1dataset"
+Data <- read_csv(url)
+
+## bring in county map data
+library(socviz)
+
+county_map |>
+  left_join(
+    county_data
+  ) |>
+  select(
+    long, lat, order, hole, piece, group, id, name, state 
+  ) -> map_data
+
+map_data |>
+  filter(state == "OH") |>
+  select(id, name) |>
+  distinct() -> map_ids
+
+Data |>
+  mutate(
+    county_name = paste0(county_name, " County")
+  ) |>
+  left_join(
+    map_ids,
+    by = c("county_name" = "name")
+  ) -> Data
+
+write_csv(
+  Data,
+  here::here(
+    "DPR 101",
+    "Data",
+    "ma1_data.csv"
+  )
+)
